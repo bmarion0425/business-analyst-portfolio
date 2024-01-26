4838984bde7edfc2c522da8143cfdab788ba0b46
bmarion0425 <108972281+bmarion0425@users.noreply.github.com>
2024-01-25 21:21:24 -0500

    Delete InsuranceDB/Dataset directory

--- InsuranceDB/Dataset/createdb.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE Policyholders (
-	policyholder_id INT primary key,
-	first_name VARCHAR (50),
-	last_name VARCHAR (50),
-	dob DATE,
-	gender VARCHAR (50),
-	contact_number VARCHAR (15),
-	email VARCHAR (100)
-);
